07be389c0c7e91a1e395d1e087d93678f1c33fe3
RustedBird <vlasov.31189@gmail.com>
2018-07-20 11:40:07 +0300

    add favicon

--- template/default.php
+++ template/default.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>FarmMak</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../img/favicon.gif" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/slick.css">
     <link rel="stylesheet" href="../css/slick-theme.css">
