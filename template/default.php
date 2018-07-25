027bfc626c02ac2bcd47ea790781284900d5a62b
RustedBird <vlasov.31189@gmail.com>
2018-07-25 22:22:00 +0300

    changed sign

--- template/default.php
+++ template/default.php
@@ -836,7 +836,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <a href="../img/теплогенератор.pdf" target="_blank">Скачать</a>
+                <a href="../img/heatgenerator.pdf" target="_blank">Скачать</a>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
